aa9a8910f5e21df0342617f422ccf7d9019633c4
DmitryPesNaumov <dm.naymov@gmail.com>
2020-12-23 17:20:21 +0400

    add key enent

--- src/renderer.js
+++ src/renderer.js
@@ -1,20 +1,56 @@
+// var canvas = document.getElementById("myCanvas");
+// var ctx = canvas.getContext("2d");
+
+// ctx.beginPath();
+// ctx.rect(70, 90, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
+
+// for (let i=0;i<10;i++){
+
+// }
+
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
+var x = 10;
+var y = 10;
+var dx_left = - 10;
+var dx_right = 10;
+var dy_up = - 10;
+var dy_down = 10;
+draw()
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+function drawRect() {
+    ctx.beginPath();
+    ctx.rect(x, y, 50, 50);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, Math.PI*2, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawRect();
+}
 
-ctx.beginPath();
-ctx.rect(160, 10, 100, 40);
-ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-ctx.stroke();
-ctx.closePath();
+document.addEventListener('keydown', function(e) {
+    switch (e.keyCode) {
+        case 37:
+            x += dx_left
+            break
+        case 38:
+            y += dy_up
+            break
+        case 39:
+            x += dx_right
+            break
+        case 40:
+            y += dy_down
+            break
+    }
+    for (let i=0;i<10;i++){
+        draw()
+    }
+    
+});
